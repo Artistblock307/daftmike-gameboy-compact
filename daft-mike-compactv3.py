bfffa795687140b79e7939f367373175cf7c63d3
Artistblock307 <randomperson542@gmail.com>
2018-08-26 22:02:13 -0500

    review rush

--- daft-mike-compactv3.py
+++ daft-mike-compactv3.py
@@ -3,53 +3,54 @@ import time
 import subprocess
 
 def main():
-    console = input("Enter console name: ")
-    rom = input("Enter rom name: ")   
 
-    procnames = ["retroarch", "ags", "uae4all2", "uae4arm", "capricerpi", "linapple", "hatari", "stella",
-                 "atari800", "xroar", "vice", "daphne", "reicast", "pifba", "osmose", "gpsp", "jzintv",
-                 "basiliskll", "mame", "advmame", "dgen", "openmsx", "mupen64plus", "gngeo", "dosbox", "ppsspp",
-                 "simcoupe", "scummvm", "snes9x", "pisnes", "frotz", "fbzx", "fuse", "gemrb", "cgenesis", "zdoom",
-                 "eduke32", "lincity", "love", "alephone", "micropolis", "openbor", "openttd", "opentyrian",
-                 "cannonball", "tyrquake", "ioquake3", "residualvm", "xrick", "sdlpop", "uqm", "stratagus",
-                 "wolf4sdl", "solarus", "emulationstation"]
-    
-    print("CART READ BEGIN")
-    
+    console = input("Enter console name: ")
+    rom = input("Enter rom name: ")
+
     #rompath = "/home/pi/RetroPie/roms/" + console + "/" + rom
-    #consolePath = "/home/john/Desktop/roms/" + console + "/"
+    #consolePath = "/home/pi/RetroPie/roms/" + console + "/"
 
     romPath = "/home/john/Desktop/roms/" + console + "/" + rom
     consolePath = "/home/john/Desktop/roms/" + console + "/"
 
     consoleOk = checkConsole(consolePath, console)
     romOk = checkRom(romPath, rom)
-    
+
+    emulators = {"nes":"nestopia", "snes":"snes9x", "n64":"mupen64plus", "gb":"gambatte", "gbc":"gambatte", "gba":"mgba", "ps1":"mednafen_psx", "psp":"ppsspp"}
+
+    if consoleOk and romOk == True:
+        os.system("retroarch -L /usr/lib/libretro/" + emulators[console] + "_libretro.so " + consolePath + "\'" + rom + "\'")
+
 def checkConsole(consolePath, console):
     if os.path.isdir(consolePath):
         consoleOk = True
-        print(console.upper() + "? ...[OK]")
+        print(console.upper() + "? ... [OK]")
+        time.sleep(1)
         return consoleOk
         
     elif console == "runScript":
         consoleOk = True
-        print("run Script? ...[OK]\n")
+        print("run Script? ... [OK]\n")
+        time.sleep(1)
         return consoleOk
             
     else:
         consoleOk = True
-        print(console.upper() + "? ...[WHAT?]\n")
+        print(console.upper() + "? ... [WHAT?]\n")
+        time.sleep(1)
         return consoleOk
 
 def checkRom(romPath, rom):
     if os.path.isfile(romPath):
         romOk = True
-        print(rom + "? ...[VALID]")
+        print(rom + "? ... [VALID]")
+        time.sleep(1)
         return romOk
 
     else:
         romOk = False
-        print(rom + " ...[FILE ERROR]\n")
+        print(rom + " ... [FILE ERROR]\n")
+        time.sleep(1)
         return romOk
 
 main()
